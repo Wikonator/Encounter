a7b77827a1d034b90ed96d41ea3edf88b40f68e7
obay <obaymardini@gmail.com>
2016-09-27 11:32:29 +0200

    get the reply button working

--- Static/modules/comments/comments.controller.js
+++ Static/modules/comments/comments.controller.js
@@ -5,8 +5,27 @@
         .module('app.s')
         .controller('CommentsController', CommentsController);
 
-    function CommentsController() {
-        console.log('hello')
+
+    function CommentsController($scope) {
+        var vm = this;
+
+        vm.reply = reply;
+
+        function reply(val) {
+            setTimeout(function() {
+                $scope.$apply()
+            },500)
+            console.log($scope)
+            var reply =angular.element("<div class="+ $(val.target).parent().attr("class") + "-1><p>hello world comment</p>" +
+            '<p id="p" class="p">reply</p></div>');
+            $(reply).on("click", vm.reply)
+            $(val.target).parent().append(reply);
+
+            //$scope.$apply();
+
+        }
+
+
     }
 
 
