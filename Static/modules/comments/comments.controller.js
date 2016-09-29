c460276567ef030c322fab6a9dffba2382f5640e
obay <obaymardini@gmail.com>
2016-09-29 16:42:05 +0200

    the comments and replies work fine with two data bindings

--- Static/modules/comments/comments.controller.js
+++ Static/modules/comments/comments.controller.js
@@ -9,20 +9,49 @@
     function CommentsController($scope) {
         var vm = this;
         vm.comment = addComment;
+        vm.newComment =Comment;
+        
         // should get the comments from the database
         vm.comments = [
             {content: 'first', class: "comments-1", id:"1", child: [{content:'first child',class: "comments-1-1", id:"1-1", child: []}, {child:[],content: "second child",class: "comments-1-1", id:"1-2"}]},
             {content: 'second',child: [{content:'first child',class: "comments-1-1", id:"1-1", child: []}, {child:[{content:'first child',class: "comments-1-1", id:"1-1-2", child: []}, {child:[],content: "second child",class: "comments-1-1", id:"1-2-2"}],content: "second child",class: "comments-1-1", id:"1-2"}], class: "comments-1", id:"2"}
         ];
         
-        $scope.add = function(data) {
+        function Comment(data,reply) {
+            console.table(data);
+            console.log(content);
             var post = data.child.length + 1;
             var newName = data.name + '-' + post;
-            data.child.push({name: newName,child: []});
+            this.content = content;
+            this.class = newName;
+            this.id = newName;
+            this.child = [];
+            this.user = "somebody";
+            this.date = new Date;  
+        }
+        $scope.like = function(data) {
+                data.likes++
+                
+        }
+        
+        $scope.disLike = function(data) {
+                data.disLikes++
+        }
+        
+        $scope.add = function(data,reply) {
+            //vm.newComment(data,content)
+            console.log(reply);
+            var post = data.child.length + 1;
+            var newName = data.id + '-' + post;
+            //data.child.push(vm.newComment(data,content));
+            //var content = content;
+            data.child.push({reply:'',likes: 0, disLikes: 0,content: reply, class: newName, id: newName, child: [], user: 'somebody', date: new Date});
+            data.reply = '';
         };
-    $scope.tree = [
-            {name: 'first', class: "comments-1", id:"1", child: [{name:'first child',class: "comments-1-1", id:"1-1", child: []}, {child:[],name: "second child",class: "comments-1-1", id:"1-2"}]},
-            {name: 'second',child: [{name:'first child',class: "comments-1-1", id:"1-1", child: []}, {child:[{name:'first child',class: "comments-1-1", id:"1-1-2", child: []}, {child:[],name: "second child",class: "comments-1-1", id:"1-2-2"}],name: "second child",class: "comments-1-1", id:"1-2"}], class: "comments-1", id:"2"}
+        
+        $scope.tree = [
+            {reply:'',likes: 0, disLikes: 0,content: 'first', class: "comments-1", id:"1", child: [{likes: 0, disLikes: 0,content:'first child',class: "comments-1-1", id:"1-1", child: []}, {likes: 0, disLikes: 0,child:[],content: "second child",class: "comments-1-1", id:"1-2"}]},
+            {likes: 0, disLikes: 0,content: 'second',child: [{likes: 0, disLikes: 0,content:'first child',class: "comments-1-1", id:"1-1", child: []}, {likes: 0, disLikes: 0,child:[{likes: 0, disLikes: 0,content:'first child',class: "comments-1-1", id:"1-1-2", child: []}, {likes: 0, disLikes: 0,child:[],content: "second child",class: "comments-1-1", id:"1-2-2"}],content: "second child",class: "comments-1-1", id:"1-2"}], class: "comments-1", id:"2"}
         ];
 
         function getComments() {
